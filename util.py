a1f0b02ef71b9c06171560b234baa5866771dc4b
unknown <rory.christian.murray@gmail.com>
2020-01-03 20:30:43 -0500

    Add stack structure

---
+++ util.py
@@ -0,0 +1,12 @@
+class Stack(self):
+	def __init__(self):
+		## storage
+		self.storage = []
+
+		## push to top
+		def push(self, item):
+			self.storage.append(item)
+
+		## pop
+		def pop(self, item):
+			return self.pop()
